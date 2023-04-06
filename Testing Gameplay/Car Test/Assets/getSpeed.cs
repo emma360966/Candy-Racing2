ecc8c59bee60eed65d7c967ba36dac77d553f372
CSAwesomeMoment <Jehanke.2411137@nv.ccsd.net>
2023-04-06 08:44:39 -0700

    attempted to make speed hud
    heavily WIP

---
+++ Testing Gameplay/Car Test/Assets/getSpeed.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class getSpeed : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    private float speed;
+    public TextMeshProUGUI speedGUI;
+
+    private Vector3 previousPosition;
+    // Update is called once per frame
+    void Update()
+    {
+         speed = playerRB.velocity.magnitude;
+         if (Input.GetKeyDown("w") || Input.GetKeyDown("s"))
+         {
+              previousPosition = transform.position;
+         }
+         
+         speed = Mathf.RoundToInt(Vector3.Distance(transform.position, previousPosition) / Time.fixedDeltaTime);
+         if (speedGUI != null)
+        {
+            speedGUI.SetText(speed.ToString());
+        }
+    }
+
+
+}
